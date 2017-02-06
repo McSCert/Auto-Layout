4fa53174269c039ec4c277c0beb34a6aada1e3e5
Gordon Marks <gor.marks@gmail.com>
2017-02-06 14:04:58 +0000

    Modified from getTextSize. Added comments, changed inputs to make it more general.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/AutoLayout@9049 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

---
+++ src/getTextWidth.m
@@ -0,0 +1,38 @@
+function width = getTextWidth(string,fontName,fontSize)
+%Get width of string
+%
+%   Inputs:
+%       string      A character array.
+%       fontName    The name of the font that string is written in.
+%       fontSize    The size of the font that string is written in.
+%
+%   Output:
+%       width       The width of a string written with the given font info.
+
+    % Check number of arguments
+    try
+        assert(nargin == 3)
+    catch
+        disp(['Error using ' mfilename ':' char(10) ...
+            ' Wrong number of arguments.' char(10)])
+        return
+    end
+    
+    % Check fontSize argument
+    try
+       assert(fontSize > 0);
+    catch
+        disp(['Error using ' mfilename ':' char(10) ...
+            ' Invalid argument: fontSize. Value must be greater than 0.' char(10)])
+        return
+    end
+
+    %Create the text in a figure and check the size of that
+    testFig = figure;
+    uicontrol(testFig)
+    x = uicontrol('Style', 'text', 'FontName', fontName, 'FontSize', fontSize);
+    set(x, 'String', string);
+    size = get(x, 'extent');
+    width = size(3);
+    close(testFig);
+end
