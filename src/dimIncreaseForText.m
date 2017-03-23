810a6bcd8316a72473c3139a126e1b9c4ce5c582
Gordon Marks <gor.marks@gmail.com>
2017-03-23 11:41:39 +0000

    Added a buffer when resizing to fit text as a workaround to cases where set_param doesn't adjust position to the designated values. Fixed a bug with varagin (didn't check for it properly). Updated comments.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/AutoLayout@9417 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/dimIncreaseForText.m
+++ src/dimIncreaseForText.m
@@ -6,49 +6,61 @@ function [pos, xIncrease] = dimIncreaseForText(block, pos, varargin)
 %       block       Full name of a block (character array).
 %       pos         The current position coordinates of the block. Expected
 %                   in same form as get_param(gcb, 'Position').
-%       varargin    Indicates how to expand block to fit its text.
+%       varargin    Indicates direction(s) to expand block to fit its text.
 %                   'left' - expand toward the left of the system.
 %                   'equal' - expand toward the left and right sides of the
 %                   system equally.
 %                   Other strings and no input result in a default of
 %                   expanding toward the right of the system.
-%                   Arguments beyond the first are ignored.
+%                   Arguments beyond the third are ignored.
 %
 %   Output:
 %       pos         New position of the block. Given in same form as
 %                   get_param(gcb, 'Position').
-%       xIncrease   Amount this block's width needed to be adjusted.
+%       xIncrease   Amount this block's width needs to be adjusted.
 
-    xIncrease = getTextXIncrease(block, pos);
+xIncrease = getTextXIncrease(block, pos);
 
-    if nargin > 0
-        if strcmp(varargin{1}, 'left') 
-            %Increase block size toward the left
-            pos(1) = pos(1) - xIncrease;
-        elseif strcmp(varargin{1}, 'equal')
-            %Increase block size left and right equally
-            pos(1) = pos(1) - xIncrease/2;
-            pos(3) = pos(3) + xIncrease/2;
-        else %Default case
-            %Increase block size toward the right
-            pos(3) = pos(3) + xIncrease;
-        end
+%Increase xIncrease marginally for blocks needing resizing
+%Rationale: set_param(<block>,'Position',<position>) sometimes resizes
+%to a smaller size than indicated, this should ensure that blocks
+%needing to be widened will be widened sufficiently
+lbound = 0; %Lower bound
+if xIncrease > lbound
+    buffer = 10;
+    xIncrease = xIncrease + buffer;
+end
+
+if nargin > 2
+    if strcmp(varargin{1}, 'left')
+        %Increase block size toward the left
+        pos(1) = pos(1) - xIncrease;
+    elseif strcmp(varargin{1}, 'equal')
+        %Increase block size left and right equally
+        pos(1) = pos(1) - xIncrease/2;
+        pos(3) = pos(3) + xIncrease/2;
+    else %Default case
+        %Increase block size toward the right
+        pos(3) = pos(3) + xIncrease;
     end
+end
 end
 
 function xIncrease = getTextXIncrease(block, pos)
-%GETTEXTXINCREASE Finds the needed amount to increase the width of block 
+%GETTEXTXINCREASE Finds the needed amount to increase the width of block
 %   in order to fit the text within it.
 %
 %   Input:
 %       block       Full name of a block (character array).
-%   
+%       pos         The current position coordinates of the block. Expected
+%                   in same form as get_param(gcb, 'Position').
+%
 %   Output:
 %       xIncrease   Amount to increase block width to fit its text.
 
-    currentWidth = pos(3) - pos(1);
+currentWidth = pos(3) - pos(1);
 
-    neededWidth = getBlockTextWidth(block);
-    
-    xIncrease = max(0, neededWidth - currentWidth);
+neededWidth = getBlockTextWidth(block);
+
+xIncrease = max(0, neededWidth - currentWidth);
 end
