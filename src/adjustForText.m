5e57a8f80f2e22cd092582dcfde323666423d0d6
Gordon Marks <gor.marks@gmail.com>
2017-03-23 11:42:08 +0000

    Update comments.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/AutoLayout@9418 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/adjustForText.m
+++ src/adjustForText.m
@@ -1,24 +1,25 @@
 function layout = adjustForText(layout)
-%ADJUSTFORTEXT Adjusts blockInfo to resize blocks to fit their text without
-%   disturbing relative layout.
+%ADJUSTFORTEXT Adjusts layout left/right positions to resize blocks to fit
+%   their text without disturbing the relative layout.
 %
 %   Inputs:
-%       blocksInfo  As returned by getLayout.
 %       layout      As returned by getRelativeLayout.
 %
 %   Output:
 %       layout      With modified position information.
 
-    for j = 1:size(layout.grid,2) % for each column
-        largestX = 0; %Shift amount
-        for i = 1:layout.colLengths(j) % for each non empty row in column
-            [layout.grid{i,j}.position, xDisplace] = dimIncreaseForText(...
-                layout.grid{i,j}.fullname,layout.grid{i,j}.position,'right'); % Returns amount to move other blocks
-            if xDisplace > largestX
-                largestX = xDisplace;
-            end
+for j = 1:size(layout.grid,2) % for each column
+    largestX = 0; %Shift amount
+    for i = 1:layout.colLengths(j) % for each non empty row in column
+        block = layout.grid{i,j}.fullname; % block to resize
+        pos = layout.grid{i,j}.position;
+        [layout.grid{i,j}.position, xDisplace] = dimIncreaseForText(...
+            block, pos, 'right'); % Returns amount to move other blocks
+        if xDisplace > largestX
+            largestX = xDisplace;
         end
-
-        layout = horzAdjustBlocks(layout, j, largestX);
     end
+    
+    layout = horzAdjustBlocks(layout, j, largestX);
+end
 end
