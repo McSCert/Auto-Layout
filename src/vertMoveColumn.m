67d78efc13987298a7e33d6a86f4fc47a622a838
Gordon Marks <gor.marks@gmail.com>
2017-03-23 11:49:27 +0000

    Updated to use the newer layout scheme instead of the old blocksMatrix scheme (no longer modifies position directly). Put in comments for the help.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/AutoLayout@9422 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/vertMoveColumn.m
+++ src/vertMoveColumn.m
@@ -1,10 +1,21 @@
-function vertMoveColumn(blocksMatrix, colLengths, row, col, y)
-% Vertically moves blocks in col and below row in blocksMatrix downward by y
+function layout = vertMoveColumn(layout, row, col, y)
+%VERTMOVECOL Vertically moves blocks in col and below row in layout.grid
+%   downward by y
+%
+%   Inputs:
+%       layout          As returned by getRelativeLayout.
+%       row             Row number. Only blocks below this row of 
+%                       layout.grid will be moved.
+%       col             Column number. Only blocks in this column of
+%                       layout.grid will be moved.
+%       y               Number of pixels to move blocks.
+%
+%   Outputs:
+%       layout      With modified position information.
 
     j = col;
-    for i = row + 1:colLengths(j)
-        pos = get_param(char(blocksMatrix{i, j}),'Position');
-        set_param(char(blocksMatrix{i, j}), 'Position', ...
-            [pos(1), pos(2) + y, pos(3), pos(4) + y]);
+    for i = row + 1:layout.colLengths(j)
+        layout.grid{i,j}.position(2) = layout.grid{i,j}.position(2) + y;
+        layout.grid{i,j}.position(4) = layout.grid{i,j}.position(4) + y;
     end
 end
