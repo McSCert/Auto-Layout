710f7d609a0bb5ca38bce2d0c90b2a7a593b7dc8
Gordon Marks <gor.marks@gmail.com>
2017-04-03 17:48:09 +0000

    Added horizontal resizing for portless blocks - some types need to be added to the cases. Switch to using the older version of vertical resizing for ports for now.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/AutoLayout@9550 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/resizeBlocks.m
+++ src/resizeBlocks.m
@@ -10,13 +10,14 @@ function [layout, portlessInfo] = resizeBlocks(layout, portlessInfo)
 %Resize horizontally to fit the strings within blocks
 layout = adjustForText(layout);
 
-%TODO Horizontally resize portless blocks for text too
-% for i = 1:length(portlessInfo)
-%     portlessInfo(i).position(3) = portlessInfo(i).position(1) + ...
-%         getBlockTextWidth(portlessInfo(i).fullname);
-% end
+%Horizontally resize portless blocks for text too
+for i = 1:length(portlessInfo)
+    portlessInfo{i}.position(3) = max(portlessInfo{i}.position(3), ...
+        portlessInfo{i}.position(1) + getBlockTextWidth(portlessInfo{i}.fullname));
+end
 
 %Resize vertically to comfortably fit ports
-layout = adjustForPorts(layout);
+%layout = adjustForPorts(layout); % Result does not consider surrounding blocks, code is much cleaner
+layout = resizeForPorts(layout); % Result considers surrounding blocks, code is less clean
 
 end
