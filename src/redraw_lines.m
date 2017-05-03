9689fc679a2aea74f552426d257cfae976c10995
Gordon Marks <gor.marks@gmail.com>
2017-05-03 22:54:57 +0000

    Fixed help comments. Improved/fixed bug in the check for the variable arguments. (Also tried to write the function using an alternative ('better') method, but it didn't work because of branching lines so it's commented out...)
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/AutoLayout@9873 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/redraw_lines.m
+++ src/redraw_lines.m
@@ -1,18 +1,40 @@
 function redraw_lines(name, varargin)
-%   Function:
-%       redraw_lines(name)
-%       redraw_lines(name, autorouting, off)
+%REDRAWBYBLOCKS Redraws all lines in system name.
+%
+%   Inputs:
+%       name            System address.
+%       varargin{1}     Set to 'autorouting' to enable use of varargin{2}.
+%       varargin{2}     Indicates whether or not the program should attempt
+%                       autorouting. Values: 'on' or 'off'. Default: 'off'.
+%   Examples:
+%       redraw_lines(name) -- redraws lines with autorouting off
+%       redraw_lines(name, 'autorouting', 'on') -- redraws lines with
+%           autorouting on
 
-    if isempty(varargin)
+    if isempty(varargin) || length(varargin) < 2
         autorouting = 'off';
     else
-        if isequal(varargin(1), {'autorouting'});
+        if isequal(varargin{1}, 'autorouting')
             autorouting = varargin{2};
         end
     end
 
-    Blocks = get_param(name,'Blocks');
+    redrawByBlocks(name,autorouting);
+%     redrawByLines(name,autorouting);
+    
+end
 
+function redrawByBlocks(name, autorouting)
+%REDRAWBYBLOCKS Redraws all lines by using get_param(name,'Blocks') and 
+%   then finding the line handles of each line on each block to find all
+%   lines.
+%
+%   Inputs:
+%       name            System address.
+%       autorouting     Indicates whether or not the program should attempt
+%                       autorouting. Values ('on' or 'off').
+
+    Blocks = get_param(name,'Blocks');
     for n = 1:length(Blocks)
         Blocks{n} = strrep(Blocks{n}, '/', '//');
         linesH = get_param([name, '/', Blocks{n}], 'LineHandles');
@@ -27,3 +49,27 @@ function redraw_lines(name, varargin)
             end
         end
     end
+end
+
+% function redrawByLines(name, autorouting)
+% %REDRAWBYLINES Redraws all lines by using get_param(name,'Lines') to find
+% %   all lines.
+% %   This method is untested, but may be better than the 'ByBlocks' method
+% %   (robustness, speed, modifiability, etc.).
+% %   UPDATE: Current implementation does not account for Branching lines
+% %
+% %   Inputs:
+% %       name            System address.
+% %       autorouting     Indicates whether or not the program should attempt
+% %                       autorouting. Values ('on' or 'off').
+% 
+%     lines = get_param(name,'Lines');
+%     for n = 1:length(lines)
+%         %src = lines(n).SrcBlock;
+%         srcport = get_param(lines(n).Handle, 'SrcPortHandle');
+%         %dest = lines(n).DstBlock;
+%         destport = get_param(lines(n).Handle, 'DstPortHandle');
+%         delete_line(lines(n).Handle);
+%         add_line(name, srcport, destport, 'autorouting', autorouting);
+%     end
+% end
