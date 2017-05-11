bc89b737cb1ad76120671ca5ba5fd7f37466f984
Gordon Marks <gor.marks@gmail.com>
2017-05-11 20:25:17 +0000

    Added handleAnnotations.m to move annotations within the system as well as a config option for it. Current options include not moving the annotations or moving them to the right of the system.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/AutoLayout@9951 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

---
+++ src/handleAnnotations.m
@@ -0,0 +1,56 @@
+function handleAnnotations(layout, portlessInfo, annotations, note_rule)
+% HANDLEANNOTATIONS Moves portless blocks to the right side of the system.
+%   The annotations should not extend too far below the bottom of the
+%   system.
+%
+%   Inputs:
+%       layout          As returned by getRelativeLayout.
+%       portlessInfo    As returned by getPortlessInfo.
+%       annotations     Vector of all of the annotations in the system.
+%       note_rule       Rule indicating what to do with annotations. See
+%                       NOTE_RULE in config.txt.
+%
+%   Outputs:
+%       N/A
+
+if strcmp(note_rule, 'on-right')
+    arbitraryBuffer = 20;
+    
+    ignorePortlessBlocks = false;
+    [~,topBound,rightBound,botBound] = sideExtremes(layout, portlessInfo, ignorePortlessBlocks);
+    
+    top = topBound;
+    right = rightBound + arbitraryBuffer;
+    
+    widest = 0;
+    
+    for i = 1:length(annotations)
+        
+        % Find width and height to maintain during repositioning
+        pos = get_param(annotations(i),'Position');
+        width = pos(3) - pos(1);
+        height = pos(4) - pos(2);
+        
+        if width > widest
+            widest = width;
+        end
+        
+        % Place annotation below previous or in top-right
+        set_param(annotations(i),'Position', [right, top, right + width, top + height])
+        
+        if top + height > botBound % New annotation column to avoid extending too far down
+            right = right + widest + arbitraryBuffer;
+            top = topBound;
+            widest = 0;
+        end
+        
+        top = top + height + arbitraryBuffer;
+        
+    end
+elseif ~strcmp(note_rule, 'none')
+    % Invalid config setting
+    disp(['Error using ' mfilename ':' char(10) ...
+        ' invalid config parameter: note_rule. Please fix in the config.txt.'])
+    return
+end % elseif 'none', then don't move the annotations at all
+end
