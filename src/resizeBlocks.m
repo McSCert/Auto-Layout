8b9cf5df479e0ce50dffd72d0463876106b7a89c
Gordon Marks <gor.marks@gmail.com>
2017-05-11 20:30:22 +0000

    Updated help comments (forgot to do so after adding portless blocks to what it would resize).
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/AutoLayout@9955 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/resizeBlocks.m
+++ src/resizeBlocks.m
@@ -2,10 +2,12 @@ function [layout, portlessInfo] = resizeBlocks(layout, portlessInfo)
 %RESIZEBLOCKS Determines desired end sizes for all blocks.
 %
 %   Inputs:
-%       layout      As returned by getRelativeLayout.
+%       layout          As returned by getRelativeLayout.
+%       portlessInfo    As returned by getPortlessInfo.
 %
 %   Output:
-%       layout      With modified position information.
+%       layout          With modified position information.
+%       portlessInfo    With modified position information.
 
 %Resize horizontally to fit the strings within blocks
 layout = adjustForText(layout);
@@ -17,7 +19,7 @@ for i = 1:length(portlessInfo)
 end
 
 %Resize vertically to comfortably fit ports
-%layout = adjustForPorts(layout); % Result does not consider surrounding blocks, code is much cleaner
-layout = resizeForPorts(layout); % Result considers surrounding blocks, code is less clean
+layout = adjustForPorts(layout); % Result does not consider surrounding blocks, code is much cleaner
+% layout = resizeForPorts(layout); % Result considers surrounding blocks, code is less clean
 
 end
