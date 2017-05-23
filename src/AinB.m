9b6c7cc2c3ea459c92fbf458da81c8676b7d89fb
Gordon Marks <gor.marks@gmail.com>
2017-05-23 23:08:53 +0000

    Edit comments and added examples with expected output.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/AutoLayout@10128 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/AinB.m
+++ src/AinB.m
@@ -1,5 +1,5 @@
 function bool = AinB(A,B)
-% AINB Determines if A is an element B.
+% AINB Determines if string A is an element in cell array B
 % There's probably a predefined MATLAB function for this that should be
 %   used instead...
 %
@@ -9,6 +9,12 @@ function bool = AinB(A,B)
 %
 %   Outputs:
 %       bool    Logical
+%
+%   Examples:
+%       AinB('a',{'a','b','c'}) -> true
+%       AinB('a',{'abc'})       -> false
+%       AinB({'a'},{{'a'}})     -> bad input results not guaranteed
+%       AinB('a',{{'a'}})       -> false
 
 bool = false;
 if ischar(A) && iscell(B)
