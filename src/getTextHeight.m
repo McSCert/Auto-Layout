3dc8735b57289318a74171b133436819a99bac48
Gordon Marks <gor.marks@gmail.com>
2017-06-09 14:19:08 +0000

    Get text height function.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/AutoLayout@10384 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/getTextHeight.m
+++ src/getTextHeight.m
@@ -1,15 +1,38 @@
-function height = getTextHeight(string, block)
+function height = getTextHeight(string,fontName,fontSize)
+%GETTEXTHEIGHT Get width of string
+%
+%   Inputs:
+%       string      A character array.
+%       fontName    The name of the font that string is written in.
+%       fontSize    The size of the font that string is written in.
+%
+%   Output:
+%       height       The height of a string written with the given font info.
 
-    fontName = get_param(block, 'FontName');
-    fontSize = get_param(block, 'FontSize');
-    if fontSize == -1
-        fontSize = get_param(bdroot, 'DefaultBlockFontSize');
+    % Check number of arguments
+    try
+        assert(nargin == 3)
+    catch
+        disp(['Error using ' mfilename ':' char(10) ...
+            ' Wrong number of arguments.' char(10)])
+        return
     end
+    
+    % Check fontSize argument
+    try
+       assert(fontSize > 0);
+    catch
+        disp(['Error using ' mfilename ':' char(10) ...
+            ' Invalid argument: fontSize. Value must be greater than 0.' char(10)])
+        return
+    end
+
+    %Create the text in a figure and check the size of that
     testFig = figure;
     uicontrol(testFig)
-    x = uicontrol('Style', 'text', 'FontName', fontName, 'FontSize', fontSize);
-    set(x, 'String', string);
-    height = get(x, 'extent');
-    height = height(4);
+    y = uicontrol('Style', 'text', 'FontName', fontName, 'FontSize', fontSize);
+    set(y, 'String', string);
+    size = get(y, 'extent');
+    height = size(4);
     close(testFig);
 end
