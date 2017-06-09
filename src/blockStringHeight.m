b65e7093fe57cf537fb4c38a04de9cbf4dc2f35c
Gordon Marks <gor.marks@gmail.com>
2017-06-09 14:19:50 +0000

    Get height of a string in block.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/AutoLayout@10385 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

---
+++ src/blockStringHeight.m
@@ -0,0 +1,10 @@
+function height = blockStringHeight(block, string)
+%BLOCKSTRINGHEIGHT Finds the height string has/would have within block
+
+    fontName = get_param(block, 'FontName');
+    fontSize = get_param(block, 'FontSize');
+    if fontSize == -1
+        fontSize = get_param(bdroot(block), 'DefaultBlockFontSize');
+    end
+    height = getTextHeight(string,fontName,fontSize);
+end
