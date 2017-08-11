52a083df80f29347767edc2c2a1cebe7173ab114
Gordon Marks <gor.marks@gmail.com>
2017-08-11 10:49:08 +0000

    Added support for Gain, ModelReference, and DocBlock blocks.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/AutoLayout@10951 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/getBlockTextWidth.m
+++ src/getBlockTextWidth.m
@@ -16,6 +16,15 @@ function neededWidth = getBlockTextWidth(block)
     
     switch blockType
         case 'SubSystem'
+            if strcmp(get_param(block, 'MaskType'), 'DocBlock')
+                docString = 'DOC';
+                docWidth = blockStringWidth(block, docString);
+                
+                docTypeString = get_param(block,'DocumentType');
+                docTypeWidth = blockStringWidth(block, docTypeString);
+                
+                neededWidth = docWidth + docTypeWidth;
+            else
             inports = find_system(block, 'SearchDepth', 1, 'LookUnderMasks', 'all', 'BlockType', 'Inport');
             
             %Don't worry about triggers for now unless an example arises in
@@ -63,7 +72,7 @@ function neededWidth = getBlockTextWidth(block)
             
             width = sum([leftWidth, rightWidth, centerWidth]);
             neededWidth = width;   %To fit different blocks of text within the block
-
+            end
         case 'If'
             ifExpression = get_param(block, 'ifExpression');
             elseIfExpressions = get_param(block, 'ElseIfExpressions');
@@ -127,8 +136,61 @@ function neededWidth = getBlockTextWidth(block)
         case 'Constant'
             string = get_param(block, 'Name');
             neededWidth = blockStringWidth(block, string);
+        
+        case 'ModelReference'
+            string = get_param(block, 'ModelName');
+            modelNameWidth = blockStringWidth(block, string);
+            
+            try
+                [inWidth, outWidth] = getModelReferencePortWidths(block);
+                defaultCenterWidth = 0;
+            catch ME
+                if strcmp(ME.identifier, 'Simulink:Commands:OpenSystemUnknownSystem')
+                    string = 'Model Not Found';
+                    defaultCenterWidth = blockStringWidth(block, string);
+                    inWidth = 0;
+                    outWidth = 0;
+                elseif strcmp(ME.identifier, 'Simulink:LoadSave:InvalidBlockDiagramName')
+                    string = 'Unspecified Model Name';
+                    defaultCenterWidth = blockStringWidth(block, string);
+                    inWidth = 0;
+                    outWidth = 0;
+                else
+                    rethrow(ME)
+                end
+            end
+            
+            cenWidth = max([modelNameWidth, defaultCenterWidth]);
+            neededWidth = sum([cenWidth, inWidth, outWidth]);
+            
+        case 'Gain'
+            string = get_param(block, 'Gain');
+            neededWidth = blockStringWidth(block, string);
             
         otherwise
             neededWidth = 0;
     end
+end
+
+function [inWidth, outWidth] = getModelReferencePortWidths(block)
+modelName = get_param(block, 'ModelName');
+load_system(modelName);
+inports = find_system(modelName, 'SearchDepth', 1, 'LookUnderMasks', 'all', 'BlockType', 'Inport');
+outports = find_system(modelName, 'SearchDepth', 1, 'LookUnderMasks', 'all', 'BlockType', 'Outport');
+
+inWidth = getBiggestNameWidth(block, inports);
+outWidth = getBiggestNameWidth(block, outports);
+
+close_system(modelName);
+end
+
+function biggestNameWidth = getBiggestNameWidth(block, objects)
+biggestNameWidth = 0;
+for i = 1:length(objects)
+    string = get_param(objects{i}, 'Name');
+    width = blockStringWidth(block, string);
+    if width > biggestNameWidth
+        biggestNameWidth = width;
+    end
+end
 end
