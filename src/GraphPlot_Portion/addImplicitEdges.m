c4bd59dc5842b9d9474fee55b064125cd3636bfc
Gordon Marks <gor.marks@gmail.com>
2017-10-13 10:51:44 +0000

    Modified code for getting implicit connections, still doesn't handle subsystems which flow into each other through contained DSwrites/DSreads/gotos/froms.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/AutoLayout@11358 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/GraphPlot_Portion/addImplicitEdges.m
+++ src/GraphPlot_Portion/addImplicitEdges.m
@@ -36,19 +36,110 @@ function dgNew = addImplicitEdges(sys, dg)
     gotos = find_system(sys, 'SearchDepth', 1, 'BlockType', 'Goto');
     froms = find_system(sys, 'SearchDepth', 1, 'BlockType', 'From');
     
-    param = cell(size(froms)); 
-    [param{:}] = deal('GotoTag');
-    fromTags = cellfun(@get_param, froms, param, 'un', 0);
-    
+% % With this implementation, the connection can be blocked if a
+% % corresponding block is within a subsystem.
+%     param = cell(size(froms)); 
+%     [param{:}] = deal('GotoTag');
+%     fromTags = cellfun(@get_param, froms, param, 'un', 0);
+%     
+%     for i = 1:length(gotos)
+%         matchIdx = find(strcmp(get_param(gotos{i}, 'GotoTag'), fromTags));
+%         for j = 1:length(matchIdx)
+%             k = matchIdx(j);
+%             gotoName = applyNamingConvention(gotos{i});
+%             fromName = applyNamingConvention(froms{k});
+%             dgNew = addedge(dgNew, gotoName, fromName, 1);
+%         end
+%     end
+
     for i = 1:length(gotos)
-        matchIdx = find(strcmp(get_param(gotos{i}, 'GotoTag'), fromTags));
-        for j = 1:length(matchIdx)
-            k = matchIdx(j);
-            gotoName = applyNamingConvention(gotos{i});
-            fromName = applyNamingConvention(froms{k});
-            dgNew = addedge(dgNew, gotoName, fromName, 1);
+        subFroms = findFromsInScope(gotos{i});
+        for j = 1:length(subFroms)
+            snk = getFirstAncestor(subFroms{j});
+            srcName = applyNamingConvention(gotos{i});
+            snkName = applyNamingConvention(snk);
+            if ~edgeExists(dgNew, srcName, snkName)
+                dgNew = addedge(dgNew, srcName, snkName, 1);
+            end
+        end
+    end
+    for i = 1:length(froms)
+        subGotos = findGotosInScope(froms{i});
+        for j = 1:length(subGotos)
+            src = getFirstAncestor(subGotos{j});
+            srcName = applyNamingConvention(src);
+            snkName = applyNamingConvention(froms{i});
+            if ~edgeExists(dgNew, srcName, snkName)
+                dgNew = addedge(dgNew, srcName, snkName, 1);
+            end
+        end
+    end
+
+    % Add Data Store Read/Writes as edges
+    writes = find_system(sys, 'SearchDepth', 1, 'BlockType', 'DataStoreWrite');
+    reads = find_system(sys, 'SearchDepth', 1, 'BlockType', 'DataStoreRead');
+
+% % With this implementation, the connection can be blocked if a
+% % corresponding block is within a subsystem.
+%     param = cell(size(reads)); 
+%     [param{:}] = deal('DataStoreName');
+%     readTags = cellfun(@get_param, reads, param, 'un', 0);
+%     
+%     for i = 1:length(writes)
+%         matchIdx = find(strcmp(get_param(writes{i}, 'DataStoreName'), readTags));
+%         for j = 1:length(matchIdx)
+%             k = matchIdx(j);
+%             writeName = applyNamingConvention(writes{i});
+%             readName = applyNamingConvention(reads{k});
+%             dgNew = addedge(dgNew, writeName, readName, 1);
+%         end
+%     end
+
+    for i = 1:length(writes)
+        subReads = findReadsInScope(writes{i});
+        for j = 1:length(subReads)
+            snk = getFirstAncestor(subReads{j});
+            srcName = applyNamingConvention(writes{i});
+            snkName = applyNamingConvention(snk);
+            if ~edgeExists(dgNew, srcName, snkName)
+                dgNew = addedge(dgNew, srcName, snkName, 1);
+            end
+        end
+    end
+    for i = 1:length(reads)
+        subWrites = findWritesInScope(reads{i});
+        for j = 1:length(subWrites)
+            src = getFirstAncestor(subWrites{j});
+            srcName = applyNamingConvention(src);
+            snkName = applyNamingConvention(reads{i});
+            if ~edgeExists(dgNew, srcName, snkName)
+                dgNew = addedge(dgNew, srcName, snkName, 1);
+            end
         end
     end
     
-    % TODO: Add Data Store Read/Writes as edges?
+    function anc = getFirstAncestor(blk)
+        % Recursively get ancestors of the block until reaching sys.
+        %
+        % For a block in sys at some depth > 1, find the subsystem at 
+        % depth == 1 which contains that block.
+        % If the block is at depth == 1, return the block.
+        
+        p = get_param(blk, 'Parent');
+        if strcmp(p, sys)
+            anc = blk;
+        else
+            anc = getFirstAncestor(p);
+        end
+    end
+
+    function exists = edgeExists(dg, source, sink)
+        exists = false;
+        for z = 1:size(dg.Edges, 1)
+            edgeFound = strcmp(source, dg.Edges{z,1}{1}) && strcmp(sink, dg.Edges{z,1}{2});
+            if edgeFound
+                exists = true;
+            end
+        end
+    end
 end
