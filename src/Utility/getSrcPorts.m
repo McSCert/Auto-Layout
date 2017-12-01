f86d436babf091f127672a14a0c07aa99513efa9
Gordon Marks <gor.marks@gmail.com>
2017-12-01 03:54:13 +0000

    Updated with version used in the logic simplifier.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/forFCA/McMaster_Tools/AutoLayout@11664 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/Utility/getSrcPorts.m
+++ src/Utility/getSrcPorts.m
@@ -1,16 +1,24 @@
-function srcPorts = getSrcPorts(block)
-% GETSRCPORTS Gets the outports that act as sources for a given block
+function srcPorts = getSrcPorts(object)
+% GETSRCPORTS Gets the outports that act as sources for a given block or
+%   dst port.
 %
 %   Input:
-%       block   Can be either the name or the handle
+%       object      Can be either the name or the handle of a block or a
+%                   port handle.
 %
 %   Output:
-%       srcPorts    Handles of ports acting as sources to block
+%       srcPorts    Handles of ports acting as sources to the object
 
     srcPorts = [];
     
-    lines = get_param(block, 'LineHandles');
-    lines = lines.Inport;
+    if strcmp(get_param(object, 'Type'), 'block')
+        block = object;
+        lines = get_param(block, 'LineHandles');
+        lines = lines.Inport;
+    elseif strcmp(get_param(object, 'Type'), 'port')
+        port = object;
+        lines = get_param(port, 'Line');
+    end
     
     for i = 1:length(lines)
         srcPorts(end+1) = get_param(lines(i), 'SrcPortHandle');
