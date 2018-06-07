f4decf64fe6512a31fe4408a869184c96655d15a
Gordon Marks <gor.marks@gmail.com>
2018-06-07 07:39:41 +0000

    Updated comments and method of handling varargin since it uses a parameter-value pair.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/AutoLayout@12970 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/redraw_lines.m
+++ src/redraw_lines.m
@@ -1,31 +1,43 @@
 function redraw_lines(sys, varargin)
-% REDRAW_LINES Redraw all lines in the system.
-%
-%   Inputs:
-%       sys             Simulink system path name.
-%       varargin{1}     Set to 'autorouting' to enable use of varargin{2}.
-%       varargin{2}     Type of automatic line routing around other blocks:
-%                       'on', 'off', 'smart'. Default is 'off'.
-%
-%   Outputs:
-%       N/A
-%
-%   Examples:
-%       redraw_lines(gcs)
-%           Redraws lines in the current system with autorouting off.
-%
-%       redraw_lines(gcs, 'autorouting', 'on')
-%           Redraws lines in the current system with autorouting on.
-
-    % Handle inputs
-    if isempty(varargin) || length(varargin) < 2
-        autorouting = 'off';
-    else
-        if isequal(varargin{1}, 'autorouting')
-            autorouting = varargin{2};
+    % REDRAW_LINES Redraw all lines in the system.
+    %
+    %   Inputs:
+    %       sys         Simulink system path name.
+    %       varargin	Parameter-Value pairs as detailed below.
+    %
+    %   Parameter-Value pairs:
+    %       Parameter: 'Autorouting' Chooses type of automatic line routing
+    %               around other blocks.
+    %       Value:  'smart' - may not work, presumably depends on MATLAB
+    %                         version.
+    %               'on'
+    %               'off' - (Default).
+    %
+    %   Outputs:
+    %       N/A
+    %
+    %   Examples:
+    %       redraw_lines(gcs)
+    %           Redraws lines in the current system with autorouting off.
+    %
+    %       redraw_lines(gcs, 'autorouting', 'on')
+    %           Redraws lines in the current system with autorouting on.
+    
+    % Handle parameter-value pairs
+    autorouting = 'off';
+    for i = 1:2:length(varargin)
+        param = lower(varargin{i});
+        value = lower(varargin{i+1});
+        
+        switch param
+            case 'autorouting'
+                assert(any(strcmp(value,{'smart','on','off'})), ['Unexpected value for ' param ' parameter.'])
+                autorouting = value;
+            otherwise
+                error('Invalid parameter.')
         end
     end
-
+    
     allBlocks = get_param(sys, 'Blocks');
     for n = 1:length(allBlocks)
         allBlocks{n} = strrep(allBlocks{n}, '/', '//');
