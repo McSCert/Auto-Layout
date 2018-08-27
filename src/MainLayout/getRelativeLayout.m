20bf9fea9f6ab5ec7f03958aba2f959c67054203
Gordon Marks <gor.marks@gmail.com>
2018-08-27 15:03:20 +0000

    Instead of using centers on x-axis to define columns, now base it off of if there's any overlap of x-axis values.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/AutoLayout@13790 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/MainLayout/getRelativeLayout.m
+++ src/MainLayout/getRelativeLayout.m
@@ -8,6 +8,82 @@ function layout = getRelativeLayout(blocks)
     %
     % Outputs:
     %   layout  Cell array where each element represents a column of blocks
+    %           (here we define a column of blocks to be the maximum set of
+    %           blocks in which every block shares an x-coordinate with any
+    %           other block in the set) represented by a cell array of block
+    %           handles ordered such that the first element has the least top
+    %           position and following elements have increasing top positions.
+    
+    blocks = inputToNumeric(blocks);
+    
+    layout = {};
+    while ~isempty(blocks) % Delete at least 1 block every loop
+        
+        % Initialize a new column with the first block.
+        layout{end+1} = {blocks(1)}; % Put column at the end as default
+        
+        % Remove first block since it has been added to a column.
+        blocks(1) = [];
+        
+        % Put the new column in the correct order with respect to other columns
+        % (further left columns get lower indices).
+        pos1 = get_param(layout{end}{1}, 'Position');
+        idx = length(layout); % init
+        for i = 1:length(layout)-1
+            col = layout{i}; % current column
+            pos2 = get_param(col{1}, 'Position');
+            if pos1(1) < pos2(1) % Compare any x-coord of one column with any x-coord of another column
+                % New column is left of the current column.
+                layout = [layout(1:i-1) layout(end) layout(i:end-1)];
+                idx = i; break; % If break removed, still need to preserve idx
+            end
+        end
+        
+        % Add other blocks to the new column
+        stillAddingBlocks = true;
+        while stillAddingBlocks
+            stillAddingBlocks = false; % Switch to true if something gets added.
+            for i = length(blocks):-1:1 % Reverse order so blocks can be deleted as it goes.
+                block = blocks(i);
+                % If current block shares an x-coord with any block already
+                % added to the column, then add current block to the column and
+                % remove from blocks list if added to a column.
+                [overlap_exists, ~] = detectOverlaps(block, layout{idx}, 'OverlapType', 'Horizontal');
+                if overlap_exists
+                    % Add block to column.
+                    layout{idx}{end+1} = block;
+                    stillAddingBlocks = true;
+                    % Remove block from blocks list.
+                    blocks(i) = [];
+                    % Put the block in the correct order such that blocks with
+                    % lower top positions get lower indices in the column).
+                    pos1 = get_param(layout{idx}{end}, 'Position');
+                    for j = 1:length(layout(idx))-1 % For every block in column except the new one.
+                        pos2 = get_param(layout{idx}{j}, 'Position');
+                        if pos1(2) < pos2(2)
+                            % Move block before the jth block in the column.
+                            layout{idx} = [layout{idx}(1:j-1) layout{idx}(end) layout{idx}(j:end-1)];
+                            break
+                        end
+                    end
+                end
+            end
+        end
+        
+        % The remaining blocks do not fit in any columns that have been made.
+    end
+end
+
+function layout = getRelativeLayout_using_x_centers(blocks)
+    % GETRELATIVELAYOUT Sort a set of blocks into columns and order within
+    % columns by their height.
+    %
+    % Inputs:
+    %   blocks  List (cell array or vector) of blocks (fullnames or
+    %           handles).
+    %
+    % Outputs:
+    %   layout  Cell array where each element represents a column of blocks
     %           (blocks centered on the same x coordinate). The column is
     %           represented by a cell array of block handles ordered such
     %           that the first element has the least top position and
@@ -15,6 +91,7 @@ function layout = getRelativeLayout(blocks)
     
     blocks = inputToNumeric(blocks);
     
+    %
     centers = position_center(get_param(blocks, 'Position'));
     x_centers = centers(:, 1);
     
