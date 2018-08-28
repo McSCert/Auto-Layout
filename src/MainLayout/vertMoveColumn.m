94092565e20fd93d6961a581e873a172990d26ad
Gordon Marks <gor.marks@gmail.com>
2018-08-28 20:49:30 +0000

    Updated version using differently structured layout variable.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/AutoLayout@13834 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/MainLayout/vertMoveColumn.m
+++ src/MainLayout/vertMoveColumn.m
@@ -1,19 +1,18 @@
-function layout = vertMoveColumn(layout, row, col, y)
-% VERTMOVECOLUMN Vertically move blocks in col and below row in layout.grid
-%   downward by y.
-%
-%   Inputs:
-%       layout      As returned by getRelativeLayout.
-%       row         Row number, below which blocks will be moved.
-%       col         Column number, in whihch blocks will be moved.
-%       y           Number of pixels to move blocks.
-%
-%   Outputs:
-%       layout      With modified position information.
-
-    j = col;
-    for i = row + 1:layout.colLengths(j)
-        layout.grid{i,j}.position(2) = layout.grid{i,j}.position(2) + y;
-        layout.grid{i,j}.position(4) = layout.grid{i,j}.position(4) + y;
+function vertMoveColumn(layout, row, col, y)
+    % VERTMOVECOLUMN Vertically move blocks in col and below row in layout
+    % downward by y.
+    %
+    % Inputs:
+    %   layout  Cell array of columns. Columns are cell arrays of blocks.
+    %   row     Row number, below which blocks will be moved.
+    %   col     Column number, in whihch blocks will be moved.
+    %   y       Number of pixels to move blocks down.
+    %
+    % Outputs:
+    % 	layout  With modified position information.
+    
+    for i = row + 1:length(layout{col})
+        pos = get_param(layout{col}{i}, 'Position');
+        set_param(layout{col}{i}, 'Position', pos + [0 y 0 y]);
     end
 end
