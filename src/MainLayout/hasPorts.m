429d6ff8d38c40dd2805a8c2dd4ecc567607e409
Gordon Marks <gor.marks@gmail.com>
2018-08-28 06:46:50 +0000

    Input now accepts many blocks and they may be given as vectors or cell arrays.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/AutoLayout@13828 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/MainLayout/hasPorts.m
+++ src/MainLayout/hasPorts.m
@@ -1,23 +1,28 @@
-function hasPorts = hasPorts(block)
-% HASPORTS Check if a block has any ports.
-%
-%   Inputs:
-%       block       Full name of a block. If a cell array is given, the first
-%                   element is used.
-%
-%   Outputs:
-%       hasPorts    Whether the block has one or more ports (1), or none (0).
-
-    % Allow cell array input
-    if iscell(block)
-        block = block{1};
-    end
-
-    % The block has no ports if its parameter PortConnectivity is empty
-    ports = get_param(block,'PortConnectivity');
-    if isempty(ports)
-        hasPorts = false;
-    else
-        hasPorts = true;
+function hasPorts = hasPorts(blocks)
+    % HASPORTS Check if a block has any ports.
+    %
+    %   Inputs:
+    %       blocks      List (cell array or vector) of blocks (full names or
+    %                   handles).
+    %
+    %   Outputs:
+    %       hasPorts    Vector of logicals. Indices correspond with the indices
+    %                   of blocks indicating whether the block has one or more
+    %                   ports (1), or none (0).
+    
+    %
+    blocks = inputToNumeric(blocks);
+    
+    %
+    hasPorts = -1*ones(1,length(blocks)); % -1 should be absent from the final output
+    for i = 1:length(blocks)
+        b = blocks(i);
+        % The block has no ports if its parameter PortConnectivity is empty
+        ports = get_param(b,'PortConnectivity');
+        if isempty(ports)
+            hasPorts(i) = false;
+        else
+            hasPorts(i) = true;
+        end
     end
 end
