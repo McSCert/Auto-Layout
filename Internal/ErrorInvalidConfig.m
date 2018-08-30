a93d89f9e1de797b06e8bea9369e5202bb851e83
Gordon Marks <gor.marks@gmail.com>
2018-08-30 07:03:47 +0000

    Decided to move the autolayout Internal folder since I'm the only one who would make use of it anyway and it clutters the folders before release.
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/AutoLayout@13923 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- Internal/ErrorInvalidConfig.m
+++
@@ -1,9 +0,0 @@
-function ErrorInvalidConfig(config)
-    % Call this if a configuration setting was given an invalid value
-    %
-    % config is the name of the config, not value.
-    
-    error(['Error using ' mfilename ':' char(10) ...
-        ' Invalid config parameter: ' config '.' char(10) ...
-        ' Please fix in the config.txt.'])
-end
--- Internal/OldFunctions/findBlkInBlksInfo.m
+++
@@ -1,9 +0,0 @@
-function idx = findBlkInBlksInfo(blxInfo, blk)
-    for z = 1:length(blxInfo)
-        if strcmp(blxInfo(z).fullname, blk{1})
-            idx = z;
-            return
-        end
-    end
-    error('Block not found in blxInfo');
-end
