d610c64dd77c8f3b699b2ce425ee3101fff74ca5
Gordon Marks <gor.marks@gmail.com>
2019-06-10 17:44:37 +0000

    Fixed bug when the handle input is empty (happens when all blocks in the initial layout have no ports).
    git-svn-id: https://www.mcscert.ca/leap/svn/trunk/sw/tools/Simulink/Tools/AutoLayout@17239 9a9db257-bd8a-4a57-83e7-af9ab8ab1356

--- src/GraphPlot_Portion/applyNamingConvention.m
+++ src/GraphPlot_Portion/applyNamingConvention.m
@@ -7,67 +7,65 @@ function name = applyNamingConvention(handle)
 %
 %   Outputs:
 %       name    Name with convention applied to it.
-
-    % Check handle argument
-    try
-        assert(~isempty(handle));
-    catch
-        error('Invalid handle.');
-    end
-
-    rows = size(handle, 1);
-    cols = size(handle, 2);
     
-    if (ischar(handle) || isnumeric(handle)) && rows == 1  % Scalar or string
-        % Handle is a single block name or block handle
-        % Return name as a char
-        type = get_param(handle, 'Type');
-        if strcmp(type, 'block')
-            % Blocks
-            oldName = getfullname(handle);
-            if iscell(oldName)
-                oldName = cell2mat(oldName);
-            end
-            name = strcat([oldName ':b']);
-        elseif strcmp(type, 'port')
-            % Block ports
-            parName = get_param(handle, 'Parent');
-            portType = get_param(handle, 'PortType');
-            portNum = get_param(handle, 'PortNumber');
-            if strcmp(portType, 'inport')
-                name = [parName ':i' num2str(portNum)];
-            elseif strcmp(portType, 'outport')
-                name = [parName ':o' num2str(portNum)];
-            else
-                error('Ports other than inports and outports are not supported.');
-            end
-        end
-    elseif ischar(handle) % && rows ~= 1
-        % Handle is a list of chars
-        % Return name as a cell array of chars
-        name = cell(rows, 1);
-        for i = 1:rows
-            name(i) = {applyNamingConvention(handle(i,:))}; % Recurse
-        end
-    elseif isnumeric(handle) % && rows ~= 1
-        % Handle is a list of handles
-        % Return name as a cell array of chars
-        name = cell(rows, cols);
-        for i = 1:rows
-            for j = 1:cols
-                name(i,j) = {applyNamingConvention(handle(i,j))}; % Recurse
-            end
-        end
-    elseif iscell(handle)
-        % Handle is a cell array
-        % Return name as a cell array with the same structure
-        name = cell(rows, cols);
-        for i = 1:rows
-            for j = 1:cols
-                name(i,j) = {applyNamingConvention(handle{i,j})}; % Recurse
-            end
-        end
+    if isempty(handle)
+        name = [];
     else
-        error('Unexpected type of input.')
+        
+        rows = size(handle, 1);
+        cols = size(handle, 2);
+        
+        if (ischar(handle) || isnumeric(handle)) && rows == 1  % Scalar or string
+            % Handle is a single block name or block handle
+            % Return name as a char
+            type = get_param(handle, 'Type');
+            if strcmp(type, 'block')
+                % Blocks
+                oldName = getfullname(handle);
+                if iscell(oldName)
+                    oldName = cell2mat(oldName);
+                end
+                name = strcat([oldName ':b']);
+            elseif strcmp(type, 'port')
+                % Block ports
+                parName = get_param(handle, 'Parent');
+                portType = get_param(handle, 'PortType');
+                portNum = get_param(handle, 'PortNumber');
+                if strcmp(portType, 'inport')
+                    name = [parName ':i' num2str(portNum)];
+                elseif strcmp(portType, 'outport')
+                    name = [parName ':o' num2str(portNum)];
+                else
+                    error('Ports other than inports and outports are not supported.');
+                end
+            end
+        elseif ischar(handle) % && rows ~= 1
+            % Handle is a list of chars
+            % Return name as a cell array of chars
+            name = cell(rows, 1);
+            for i = 1:rows
+                name(i) = {applyNamingConvention(handle(i,:))}; % Recurse
+            end
+        elseif isnumeric(handle) % && rows ~= 1
+            % Handle is a list of handles
+            % Return name as a cell array of chars
+            name = cell(rows, cols);
+            for i = 1:rows
+                for j = 1:cols
+                    name(i,j) = {applyNamingConvention(handle(i,j))}; % Recurse
+                end
+            end
+        elseif iscell(handle)
+            % Handle is a cell array
+            % Return name as a cell array with the same structure
+            name = cell(rows, cols);
+            for i = 1:rows
+                for j = 1:cols
+                    name(i,j) = {applyNamingConvention(handle{i,j})}; % Recurse
+                end
+            end
+        else
+            error('Unexpected type of input.')
+        end
     end
 end
